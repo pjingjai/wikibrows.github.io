645a164570fc68d9241c35b4f9e7dd29ed467ad3
pun <pjingjai@gmail.com>
2020-02-06 22:55:26 +0700

    Initial commit

---
+++ precache-manifest.bc92f7d87d72f87f1c84d0c4de801ad0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72721c2428a1db17e1812fef51cc0d05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3d04158d2a903b10216",
+    "url": "/static/css/2.78096a31.chunk.css"
+  },
+  {
+    "revision": "62d5b365b6fdb9e00e47",
+    "url": "/static/css/main.fff926c9.chunk.css"
+  },
+  {
+    "revision": "d3d04158d2a903b10216",
+    "url": "/static/js/2.8f3b22a7.chunk.js"
+  },
+  {
+    "revision": "10043301bdecbc14a5b2d3616d0590c0",
+    "url": "/static/js/2.8f3b22a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62d5b365b6fdb9e00e47",
+    "url": "/static/js/main.15e2469c.chunk.js"
+  },
+  {
+    "revision": "ce4c0a2a58d7919fcac7",
+    "url": "/static/js/runtime-main.34278baa.js"
+  },
+  {
+    "revision": "140f3035346c474d9ee9a4e576c1bf71",
+    "url": "/static/media/enwiktionary.140f3035.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "d3aa9e70dc0361a222470afb329ac5ed",
+    "url": "/static/media/wikipedia.d3aa9e70.jfif"
+  }
+]);
